27616e690ebcdfed6af0ea2819ea97c170edf35c
Bhaskor Jyoti Sarmah <bhaskor.sarmah@bohniman.com>
2018-08-13 21:06:04 +0530

    All Static Resources were Loaded Successfully.

--- onlineshopping/src/main/java/net/bjs/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/bjs/onlineshopping/controller/PageController.java
@@ -1,6 +1,7 @@
 package net.bjs.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,29 +9,32 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
-	@RequestMapping(value = {"/", "/home","/index"})
-	public ModelAndView index() {
+	@RequestMapping(value = {"/","/home","/index"})
+	public ModelAndView getRoot() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring Web MVC");
 		return mv;
 	}
 	
-	@RequestMapping(value = "/**")
-	public ModelAndView error() {
-		ModelAndView mv = new ModelAndView("error");
-		mv.addObject("greeting","Error ! Page not found");
-		return mv;
-	}
-	
-
-	@RequestMapping(value = "/test")
-	public ModelAndView getGreetings(@RequestParam(value="message", required=false) String greeting) {
-		ModelAndView mv = new ModelAndView("page");
-		if(null==greeting) {
-			greeting  = "Hello There !";
-		}
-		mv.addObject("greeting",greeting);
-		return mv;
-	}
+//	@RequestMapping(value = "/test")
+//	public ModelAndView getGreetings(@RequestParam(value="message", required=false) String greeting) {
+//		ModelAndView mv = new ModelAndView("page_test");
+//		if(null==greeting) {
+//			greeting  = "Hello There !";
+//		}
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+//	
+//
+//
+//	@RequestMapping(value = "/test/{message}")
+//	public ModelAndView getGreetingsFromPath(@PathVariable("message") String greeting) {
+//		ModelAndView mv = new ModelAndView("page_test");
+//		if(null==greeting) {
+//			greeting  = "Hello There !";
+//		}
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
 	
 }
